97353490ff58a7396b34a84b754317d665a452a5
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-08-21 15:46:53 +0300

    Added project with boxing example

---
+++ CS.Itself.Boxing/CS.Itself.Boxing/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CS.Itself.Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 234;
+            var box = new Box<int>(i);
+            Console.WriteLine(box);
+            Console.WriteLine(box.GetType());
+
+            Console.WriteLine();
+
+            int j = 234;
+            object o = j;
+
+            Console.WriteLine(o);
+            Console.WriteLine(o.GetType());
+        }
+    }
+
+    sealed class Box<T>
+    {
+        private T value;
+        public Box(T t)
+        {
+            value = t;
+        }
+
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+    }
+}
